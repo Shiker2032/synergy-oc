24fc2fab26e26c65dbbff0229aea266f36c60cdc
Vlad Begunov <53581923+Shiker2032@users.noreply.github.com>
2023-05-29 13:25:09 +0300

    add attachments dir creation

--- delete.sh
+++ delete.sh
@@ -1,2 +1,1 @@
-cd ./results
-rm *.png
+rm -R attachments
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-cd ./results
+mkdir attachments
+cd ./attachments
 for ((i=1; i <= 20; i++))
 do
 curl https://i.pravatar.cc/800/400 -L > photo$i.png
