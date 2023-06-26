a85187c7c1cf01ced76340f6c5de012fd1a4a891
Vlad Begunov <53581923+Shiker2032@users.noreply.github.com>
2023-06-26 18:23:07 +0300

    add solution

---
+++ app.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+
+bool checkLat(double lat) {
+	if ((lat >= 50) && (lat <= 80)) {
+		return true;
+	}
+	else return false;
+}
+
+bool checkLon(double lon) {
+
+	if ((lon >= 20) && (lon <= 45)) {
+		return true;
+	}
+	else return false;
+}
+
+
+
+int main() 
+{
+
+	std::ifstream ifile("coordinates.txt", std::ios::in);
+	if (ifile.is_open()) {
+		while (ifile) {
+			std::string line;
+			std::getline(ifile, line);			
+			std::stringstream ss;
+			ss << line;			
+			
+			double lon, lat = 0;
+			ss.ignore(1);
+			ss >> lat;
+			
+			
+			ss.ignore(1);
+			ss >> lon;
+
+			if (checkLat(lat) && (checkLon(lon))) {
+				std::cout << lat << ", " << lon << std::endl;
+			}
+		
+		}
+	}
+}
